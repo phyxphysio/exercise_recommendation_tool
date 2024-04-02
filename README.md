1987663bfa29f09e82c8ef51dba58ab7ef875923
Liam <20miller13@gmail.com>
2024-04-02 11:05:27 +1100

    Update README.md, add requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# exercise_recommendation_tool
-This python program helps users find appropriate exercises. users can get exercsie reccommendations in two ways: Be shown exercises that are siilar to one they like, or be shown a list of exercsies based on the body part they would like to target. 
-The program uses pandas, numpy, and scikit learn to compare and reccommend exercsies. 
+# Exercise Recommendation Tool
+
+## Introduction
+
+This python program helps users find appropriate exercises. users can get exercise recommendations in two ways: Be shown exercises that are similar to one they like, or be shown a list of exercise based on the body part they would like to target.
+The program uses pandas, Numpy, and Scikit learn to compare and recommend exercises.
+
+## Getting Started
+
+### Clone the repo
+
+```bash
+git clone https://github.com/phyxphysio/exercise_recommendation_tool
+```
+
+### Install requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+### Run file with python
+
+```bash
+python main.py
+```
+
+### Follow prompts in terminal
+
+You can choose to receive exercise recommendations based on:
+
+1. An exercise you already like
+2. The part of the body you'd like to work
+
+After receiving your recommendation, you can choose to go again, or to open a link to a YouTUbe channel with guided videos of the exercises.
--- main.py
+++ main.py
@@ -1,63 +1,70 @@
-# Import libaries 
-from wsgiref.validate import PartialIteratorWrapper
 import pandas as pd
-import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 import webbrowser
-# Store data 
-df = pd.read_csv('/Users/luammiller/Desktop/Code/Portfolio Project 2/phyx_exs_data.csv')
 
-# Create string with exercise qualities that allows u to compare  
+# Store data
+df = pd.read_csv("phyx_exs_data.csv")
+df['Body Part'] = [' '.join(part.split('-')).capitalize() for part in df['Body Part']]
+
+# Create string with exercise qualities that allows u to compare
 def combine_qualities(data):
-    return [data["Difficulty"][i] + " "+ data[ "Body Part"][i] + " "+ data["Equipment"][i] for i in range(data.shape[0])]
+    return [
+        data["Difficulty"][i] + " " + data["Body Part"][i] + " " + data["Equipment"][i]
+        for i in range(data.shape[0])
+    ]
 
-# Create a column to hold combined qualities 
+
+# Create a column to hold combined qualities
 df["combined_qualities"] = combine_qualities(df)
 
-# Convert combined qualites string into token matrix
+# Convert combined qualities string into token matrix
 cm = CountVectorizer().fit_transform(df["combined_qualities"])
 
-# Get cosine similarity from token matrix 
+# Get cosine similarity from token matrix
 cs = cosine_similarity(cm)
 
 # Add exercise id to df
 df["exs_id"] = list(range(df.shape[0]))
 
-#Introcduce the program an dchoose reccommendation method 
+
+# Introduce the program an choose recommendation method
 def intro():
     print("Welcome to the Phyx Exercise Prescription Algorithm! \n")
-    reccommendation_method = None
-    while reccommendation_method not in ["1", "2"]:
-        reccommendation_method = input("You can recieve reccommendations based on:\n1 An exercise you like\n2 The part of the body you'd like to work\nType the number of the option you'd like to select: ")
-        if reccommendation_method not in ["1", "2"]:
-            print(f"\n'{reccommendation_method}' isn't an option. Type '1' or '2'\n")
-    if reccommendation_method == "1":
+    recommendation_method = None
+    while recommendation_method not in ["1", "2"]:
+        recommendation_method = input(
+            "You can receive recommendations based on:\n1 An exercise you like\n2 The part of the body you'd like to work\nType the number of the option you'd like to select: "
+        )
+        if recommendation_method not in ["1", "2"]:
+            print(f"\n'{recommendation_method}' isn't an option. Type '1' or '2'\n")
+    if recommendation_method == "1":
         print_score()
         try_again()
-    elif reccommendation_method == "2":
+    elif recommendation_method == "2":
         display_exercises_in_region(get_body_part())
-    
+
 
 # Get exercise from user
 def get_input():
     print(df.Name.to_string())
-    exs_id = int(input("Type its number of the exercise to see similar ones:"))
+    exs_id = int(input("Type the number of the exercise to see similar ones:"))
     while exs_id not in range(df.shape[0]):
         print("That's not a valid option. Try again.")
         get_input()
     return exs_id
 
+
 # Compare and display top matches
 def print_score():
     exs_id = get_input()
 
-    # Create enumerated list of similr exercises 
+    # Create enumerated list of similar exercises
     scores = list(enumerate(cs[exs_id]))
 
     # Sort the list of scores
-    sorted_scores = sorted(scores, key = lambda x:x[1], reverse=True)
-    
+    sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
     # Remove base exercise from list of matches
     for i in sorted_scores:
         if i[0] == exs_id:
@@ -68,13 +75,12 @@ def print_score():
     j = 0
     for i in sorted_scores:
         rec_name = df[df.exs_id == i[0]]["Name"].values[0]
-        rec_score = str(int(i[1]*100)) + "%"
-        print(j+1, rec_name, rec_score)
-        j+=1
-        if j>5:
+        rec_score = str(int(i[1] * 100)) + "%"
+        print(j + 1, rec_name, rec_score)
+        j += 1
+        if j > 5:
             break
 
-# print_score()
 
 # Allow the user ot keep comparing
 def try_again():
@@ -88,100 +94,110 @@ def try_again():
         try_again()
 
 
-# Add reccommendation by category functionality 
+# Add recommendation by category functionality
 body_part_column = df["Body Part"]
 
+
 # Create list of body parts for user to chose from
-def listify_strings(coulmn):
-    return [i.split(",") for i in coulmn]
+def listify_strings(column):
+    return [i.split(",") for i in column]
+
+
 df["regions_listed"] = listify_strings(body_part_column)
-    
+
+
 def compile_part_list(column):
-    part_list= []
+    part_list = []
     cleaned_regions_list = []
     for i in range(len(column)):
         cleaned_regions_list.append([])
         for region in column[i]:
-            region = region.replace(" ","")
+            region = region.replace(" ", "")
             region = region.title()
             cleaned_regions_list[i].append(region)
             if region not in part_list:
                 part_list.append(region)
             else:
-                continue 
-            
+                continue
+
     return part_list, cleaned_regions_list
+
 
 body_part_list = compile_part_list(df["regions_listed"])[0]
 cleaned_regions_column = compile_part_list(df["regions_listed"])[1]
 
-# Use autocomplete to help user choose category 
+
+# Use autocomplete to help user choose category
 def get_first_letter():
-    return input("\nWhich body part would you like to work?\nType the first letter to see the corresponding options.\n")      
+    return input(
+        "\nWhich body part would you like to work?\nType the first letter to see the corresponding options.\n"
+    )
+
+
 def get_body_part():
     first_letter_of_category = get_first_letter().lower()
     j = -1
     region_with_first_letter = []
     for region in body_part_list:
         if region[0].lower() == first_letter_of_category:
-            j+=1
+            j += 1
             region_with_first_letter.append(region)
             print(j, region)
-    
-    # If the user does not enter a valid letter, prompt agan with a list of options
+
+    # If the user does not enter a valid letter, prompt adan with a list of options
     if j < 0:
-        print(f"\nWe couldn't find any categories starting with '{first_letter_of_category}'.\nHere's a list of the categories have:\n" )
+        print(
+            f"\nWe couldn't find any categories starting with '{first_letter_of_category}'.\nHere's a list of the categories have:\n"
+        )
         for region in body_part_list:
-            j+=1
-            print(j,region)
+            j += 1
+            print(j, region)
             region_with_first_letter.append(region)
 
-        
-    
-    # Allow the user to choose from the subset they hve chosen 
-    
+    # Allow the user to choose from the subset they hve chosen
+
     region_choice = None
     while region_choice not in range(len(region_with_first_letter)):
-        try: region_choice = int(input("Type the number of the body part you'd like t work."))
-        except ValueError as ve:
+        try:
+            region_choice = int(
+                input("Type the number of the body part you'd like t work.")
+            )
+        except ValueError:
             print("Type a number.")
     return region_with_first_letter[int(region_choice)]
 
+
 # Display the exercises in the category chosen by the user
 def display_exercises_in_region(region):
-    rows_to_keep = [i for i in range(len(cleaned_regions_column)) if region in cleaned_regions_column[i]]
+    rows_to_keep = [
+        i
+        for i in range(len(cleaned_regions_column))
+        if region in cleaned_regions_column[i]
+    ]
     exs_to_display = df.drop([i for i in range(df.shape[0]) if i not in rows_to_keep])
-    exs_to_display = exs_to_display.drop(["exs_id", "regions_listed", "combined_qualities", "Body Part"], 1)
+    exs_to_display = exs_to_display.drop(
+        ["exs_id", "regions_listed", "combined_qualities", "Body Part"], axis=1
+    )
     print(f"Here are the exercises that work the {region}.\n")
     print(exs_to_display)
     try_again()
 
-#Allow user to open exercise video on youtube
+
+# Allow user to open exercise video on youtube
 def see_videos():
     choice = None
-    while choice not in ['y','n']:
-        choice = input("\nWould you lke to see videos of these exercises in your web browser?\nType 'y' or 'n' to select: ")
-        if choice not in ['y','n']:
+    while choice not in ["y", "n"]:
+        choice = input(
+            "\nWould you lke to see videos of these exercises in your web browser?\nType 'y' or 'n' to select: "
+        )
+        if choice not in ["y", "n"]:
             print(f"\n{choice} isn't an option.")
-    if choice == 'y':
-        webbrowser.open('https://www.youtube.com/playlist?list=PLpqyTnl8cO1m3kL5goWP45PKkz1QfxRwF')
-    if choice == 'n':
+    if choice == "y":
+        webbrowser.open(
+            "https://www.youtube.com/playlist?list=PLpqyTnl8cO1m3kL5goWP45PKkz1QfxRwF"
+        )
+    if choice == "n":
         print("Okay, see ya next time!")
 
 
 intro()
-
-
-
-
-        
-
-
-
-        
-    
-
-
-    
-
-    
